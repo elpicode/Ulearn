134536208b031a33b59662c1deccc454b6fa481d
Eleonora Pikalo <70911764+elpicode@users.noreply.github.com>
2021-02-05 14:33:05 +0500

    Add files via upload

---
+++ 1. Ошибки/AngryBirdsTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AngryBirds
+{
+    public static class AngryBirdsTask
+    {
+       
+        public static double FindSightAngle(double v, double distance)
+        {
+            var g = 9.8;
+            var sin2a = (distance * g) / (v * v);
+            return Math.Asin(sin2a) / 2;
+        }
+    }
+}
---
+++ 1. Ошибки/BilliardsTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Billiards
+{
+    public class BilliardsTask
+    {
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="directionRadians">Угол направелния движения шара</param>
+            /// <param name="wallInclinationRadians">Угол</param>
+            /// <returns></returns>
+            return wallInclinationRadians - directionRadians + wallInclinationRadians;
+        }
+    }
+}
